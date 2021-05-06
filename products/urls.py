6d0979a30e91828826365d58760534c4691220e0
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2021-05-06 23:17:35 +0000

    product view is more dynamic

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.view, name="products-index"),
-    path("<str:category>", views.view, name="products-list"),
+    path("", views.overview),
+    path("<str:category>", views.view),
 ]
--- products/views.py
+++ products/views.py
@@ -1,18 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
 
 
 # Create your views here.
 
-def view(request, category=None):
-    if category in {'breakfast', 'milk', 'tableware'}:
+def overview(request):
+    return render(request,
+                  'products/index.html',
+                  context={'categories': Category.objects.all()},
+                  )
+
+
+def view(request, category: str = None):
+    if category.lower() in {'breakfast', 'milk', 'tableware', 'all'}:
         return render(request,
                       'products/product-list.html',
                       context={'category': category,
-                               'products': Product.objects.filter(category__name__contains=category)}
+                               'products': Product.objects.all() if category.lower() == 'all' else Product.objects.filter(
+                                   category__name__contains=category)}
                       )
     else:
-        return render(request,
-                      'products/index.html',
-                      context={'category': None},
-                      )
+        return HttpResponse(f'404: There is no category {category}', status=404)
