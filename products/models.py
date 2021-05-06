7e52acdab5a803c71d22de7586903bad46b2c9b9
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2021-05-06 12:41:16 +0000

    removed misc.xml, added __str__ methods

--- products/models.py
+++ products/models.py
@@ -6,6 +6,9 @@ from django.db.models import CharField, DecimalField
 class ProductCategory(models.Model):
     name = CharField(max_length=64)
 
+def __str__(self):
+    return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
@@ -13,6 +16,9 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     price = DecimalField(max_digits=10, decimal_places=2)
     on_sale = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
 
 class ProductImage(models.Model):
