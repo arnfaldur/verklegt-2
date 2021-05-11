add193bf75d3b3a4ade940dbbb59151ac91e8a9c
siggavala <sigga-vala@hotmail.com>
2021-05-11 15:56:39 +0000

    cart

--- products/forms/product_form.py
+++ products/forms/product_form.py
@@ -2,15 +2,15 @@ from django.forms import ModelForm, widgets
 from django import forms
 from products.models import Product, Image
 
+
 class ProductCreateForm(ModelForm):
     imgage = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     class Meta:
         model = Product
         exclude = [ 'id' ]
-        widgets ={
+        widgets = {
             'name': widgets.TextInput(attrs={'class': 'form-control'}),
             'description': widgets.TextInput(attrs={'class': 'form-control'}),
-            'type': widgets.TextInput(attrs={'class': 'form-control'}),
             'price': widgets.NumberInput(attrs={'class': 'form-control'}),
             'on_sale': widgets.CheckboxInput(attrs={'class': 'checkbox'}),
         }
