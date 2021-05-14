d525b13784b342d4e6d3fd4f38ab3b9d006c5c35
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2021-05-14 23:15:58 +0000

    fixed admin page with custom user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,41 @@
+import django.contrib.auth.admin
 from django.contrib import admin
+from django.utils.translation import gettext, gettext_lazy as _
 
-# Register your models here.
+from user.models import User
+
+
+# class GroupAdmin(django.contrib.auth.admin.GroupAdmin):
+#     ordering = ()
+#     pass
+
+
+class UserAdmin(django.contrib.auth.admin.UserAdmin):
+
+    add_form_template = 'admin/auth/user/add_form.html'
+    change_user_password_template = None
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    # form = UserChangeForm
+    # add_form = UserCreationForm
+    # change_password_form = AdminPasswordChangeForm
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+
+admin.site.register(User, UserAdmin)
